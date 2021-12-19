91b382cb5ac65216d8ca68aadff802d76976ca34
Wilhelm Schnepp <wilhelm.schnepp@stud-mail.uni-wuerzburg.de>
2021-12-19 23:43:52 +0100

    UnityToJSON: Add helper scripts

---
+++ UnityToJSON/Assets/TextRenderer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class TextRenderer : MonoBehaviour
+{
+    public string text;
+    public int fontSize;
+    public Color textColor;
+}
