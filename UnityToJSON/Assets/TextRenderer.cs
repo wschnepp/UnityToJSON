ff576891ebf5bbb273c2554e9cc35287a6c950a0
Wilhelm Schnepp <wilhelm.schnepp@stud-mail.uni-wuerzburg.de>
2021-12-20 15:53:16 +0100

    UnityToJSON: Control size of child plane instead, unifying dimensions with EGBR

--- UnityToJSON/Assets/TextRenderer.cs
+++ UnityToJSON/Assets/TextRenderer.cs
@@ -15,21 +15,26 @@ public class TextRenderer : MonoBehaviour
 
     public int vres;
 
+    GameObject childPlane = null;
 
     public void OnValidate()
     {
+        childPlane = transform.GetChild(0)?.gameObject;
 
-        var textAspectRatio = hres / (float)vres;
-        const float meshScale = 10; //unity default plane is 10*10 meters by default
-        const float onePoint = 0.352f * 10e-3f; // one point == 1/72 inch or 0.35 mm
-        float worldFontSize = (fontSize * onePoint) / meshScale;
+        if (childPlane)
+        {
+            var textAspectRatio = hres / (float)vres;
+            const float meshScale = 10; //unity default plane is 10*10 meters by default
+            const float onePoint = 0.352f * 10e-3f; // one point == 1/72 inch or 0.35 mm
+            float worldFontSize = (fontSize * onePoint) / meshScale;
 
 
-        var horizontalSize = hres * worldFontSize;
-        var scale = transform.localScale;
-        scale.x = !float.IsNaN(textAspectRatio) ? textAspectRatio * horizontalSize : 0;
-        scale.z = horizontalSize;
+            var verticalSize = vres * worldFontSize;
+            var scale = new Vector3(0, 1, 0);
+            scale.x = !float.IsNaN(textAspectRatio) ? textAspectRatio * verticalSize : 0;
+            scale.z = verticalSize;
 
-        transform.localScale = scale;
+            childPlane.transform.localScale = scale;
+        }
     }
 }
